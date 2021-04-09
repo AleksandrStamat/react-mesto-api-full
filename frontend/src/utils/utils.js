37bf1d0a097d0b7b36682b3ea98e9ed5a634237d
Александр <aleksandr@MacBook-Pro-Aleksandr.local>
2021-04-09 18:56:39 +0300

    little change

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,8 +1,7 @@
 import Api from './api';
 
 export const baseUrl = 'https://api.mesto.stamat.nomoredomains.icu';
-// export const baseUrl = 'http://localhost:3000';
-
+// export const baseUrl = 'http://localhost:3000';// учзщке сщтые ифыуГкд = эреезЖ//дщсфдрщыеЖ3000эж
 
 
 export const api = new Api({ baseUrl });
