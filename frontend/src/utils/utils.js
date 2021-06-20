2075724d1fe6c8dbc96ffc4ae6fa9af28099b6c4
Александр <aleksandr@MacBook-Pro-Aleksandr.local>
2021-06-20 16:33:13 +0300

    fihish project

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,7 +1,7 @@
 import Api from './api';
 
-export const baseUrl = 'https://api.mesto.stamat.nomoredomains.icu';
-// export const baseUrl = 'http://localhost:3000';
+// export const baseUrl = 'https://mesto.astamat.ru/api';
+export const baseUrl = 'http://localhost:3000';
 
 
 export const api = new Api({ baseUrl });
